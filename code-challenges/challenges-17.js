dce7c5cac3dbed7d1d94dfc72a5dbdc0d8653a45
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-04-04 22:02:01 +0300

    exe17

---
+++ code-challenges/challenges-17.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const { Console } = require("console");
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// referring to the given examples find out the pattern used and
+// Write a function that takes 2 integers and prints the patterns using recursion
+// 
+// Input: 16, 5
+// Output: [16, 11, 6, 1, -4, 1, 6, 11, 16]
+//
+// Input: 50, 9
+// Output: [50, 41, 32, 23, 14, 5, -4, 5, 14, 23, 32, 41, 50]
+//
+
+const recursionPattern = (int1, int2) => {
+    let max = int1;
+    let flag = 0;
+    let arr = [];
+
+    while (1) {
+        if (flag == 0) {
+            arr.push(int1);
+            int1 -= int2;
+        }
+        else {
+            arr.push(int1);
+            int1 += int2;
+        }
+
+        if (int1 <= 0) {
+            flag = 1;
+        }
+        if (int1 > max) {
+            break;
+        }
+    }
+    return arr;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+// 
+// Write a function that takes a string (HTML tag)
+// and extracts the link from the HTML tag
+//
+// Ex: <a href="http://www.hackerrank.com"><h1><b>HackerRank</b></h1></a> ==> "www.hackerrank.com"
+// Ex: <a href="http://www.something.org"><h1><b>HackerRank</b></h1></a> ==> "www.something.org"
+//
+// Note:
+//  Assume that links end with .com, .org or .net
+// 
+
+const filterLinks = (str) => {
+    let arr = str.split('"');
+    arr = arr[1].split("http://");
+    return arr[1];
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Required:
+// 
+// A phrase is considered palindrome if, after converting all uppercase letters into lowercase letters
+// and removing all non-alphanumeric characters, it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// 
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// as you can see "amanaplanacanalpanama" is a palindrome.
+//
+
+const isPalindrome = (str) => {
+    let arr=str.split('').filter(char => /[a-zA-Z]/.test(char));;
+    
+    str=arr.join("");
+    let temp=str.toLowerCase();
+    console.log(temp);
+    let begin = 0, end = temp.length - 1;
+
+    while (begin <= end) {
+        begin++;
+        end--;
+        if (temp[begin] != temp[end]) {
+            return false;
+        }
+        console.log(begin ," " ,end)
+    }
+    return true;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+// 
+//  Write a function that takes 2 arguments, one is a string and the other is an array
+//  the function should return a boolean than indicates if both have the same pattern
+//
+//  EX: 
+//  Input: "abba", ['cat', 'dog', 'dog', 'cat']
+//  output: true
+//
+//  as you can see the a acted as the cat in the array, and b as the dog, and the pattern was the same
+//
+//  Input: "cda", ['cat', 'dog', 'cat']
+//  output: false
+//
+//  here if the pattern had three different things so the array should had 3 different things to be true
+//
+
+const samePattern = (str, arr) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+module.exports = { recursionPattern, filterLinks, isPalindrome, samePattern };
